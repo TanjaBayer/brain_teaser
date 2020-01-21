7c1349ab250ff08e604833459af1a359a7fffe48
Tanja Bayer <tanja.bayer@cubesoft.org>
2020-01-21 21:22:10 +0100

    recursive calculation for n > 1

--- task_a.py
+++ task_a.py
@@ -2,8 +2,19 @@
 def calc_sequence(m, n, old_val):
     if n == 1:
         return m
+    if not old_val:
+        old_val = calc_sequence(m, n - 1, None)
     if old_val % 2:
         return old_val * 3 + 1
     return old_val/2
 
+def run_sequence(m, n):
+    result = None
+    while result != 1:
+        result = calc_sequence(m, n, result)
+        n += 1
+    return n - 1
 
+
+if __name__ == '__main__':
+    pass
--- tests/test_task_a.py
+++ tests/test_task_a.py
@@ -1,6 +1,6 @@
 import pytest
 
-from ..task_a import calc_sequence
+from ..task_a import calc_sequence, run_sequence
 
 
 @pytest.mark.parametrize("m, n", [(1, 1), (1000, 1), (50, 1)])
@@ -16,3 +16,12 @@ def test_cm_is_multiplied_by_3_summed_with_1_for_uneven_values_of_cm(m, n, resul
 @pytest.mark.parametrize("m, n, result", [(2, 2, 1), (80, 2, 40), (80006, 2, 40003)])
 def test_cm_is_devided_by_two_for_even_values_of_cm(m, n, result):
     assert calc_sequence(m, n, m) == result
+
+
+# @pytest.mark.parametrize("m, n, result", [(1, 1, 1), (5, 1, 5), (20, 5, )])
+# def test_run_sequence_retruns_correct_value_for_steps(m, n, result):
+#     assert run_sequence(m, n) == result
+
+@pytest.mark.parametrize("m, n, result", [(2, 2, 1), (20, 5, 8), (13, 7, 8)])
+def test_calc_sequence_if_not_starting_with_n_equals_1(m, n, result):
+    assert calc_sequence(m, n, None) == result
