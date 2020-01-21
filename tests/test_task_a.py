48c523e6e7eda7350cf6bd7a28ae44c1b094801d
Tanja Bayer <tanja.bayer@cubesoft.org>
2020-01-21 21:25:07 +0100

    tests for run_sequence with number of steps

--- tests/test_task_a.py
+++ tests/test_task_a.py
@@ -18,9 +18,10 @@ def test_cm_is_devided_by_two_for_even_values_of_cm(m, n, result):
     assert calc_sequence(m, n, m) == result
 
 
-# @pytest.mark.parametrize("m, n, result", [(1, 1, 1), (5, 1, 5), (20, 5, )])
-# def test_run_sequence_retruns_correct_value_for_steps(m, n, result):
-#     assert run_sequence(m, n) == result
+@pytest.mark.parametrize("m, n, result", [(1, 1, 1), (5, 1, 6), (20, 5, 8)])
+def test_run_sequence_retruns_correct_value_for_steps(m, n, result):
+    assert run_sequence(m, n) == result
+
 
 @pytest.mark.parametrize("m, n, result", [(2, 2, 1), (20, 5, 8), (13, 7, 8)])
 def test_calc_sequence_if_not_starting_with_n_equals_1(m, n, result):
