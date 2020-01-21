e717e36ced8dc5764b8d4b2ab7fe69a6e26bc498
Tanja Bayer <tanja.bayer@cubesoft.org>
2020-01-21 19:40:54 +0100

    added test for unevenvalues of m and n==2

--- task_a.py
+++ task_a.py
@@ -1,3 +1,7 @@
 
-def calc_sequence(m, n):
-    return m
+def calc_sequence(m, n, old_val):
+    if n == 1:
+        return m
+    if old_val % 2:
+        return old_val * 3 + 1
+
--- tests/test_task_a.py
+++ tests/test_task_a.py
@@ -5,5 +5,11 @@ from ..task_a import calc_sequence
 
 @pytest.mark.parametrize("m, n", [(1, 1), (1000, 1), (50, 1)])
 def test_cm_is_m_for_cm_of_1(m, n):
-    
-    calc_sequence(m, n)
+    calc_sequence(m, n, 0)
+
+
+@pytest.mark.parametrize("m, n, result", [(5, 2, 16), (11, 2, 34), (1000001, 2, 3000004)])
+def test_cm_is_devided_by_two_for_uneven_values_of_cm(m, n, result):
+    assert calc_sequence(m, n, m) == result
+
+
