301df7076a906b10d2f6a3ad671bfa5162f9ea24
Tanja Bayer <tanja.bayer@cubesoft.org>
2020-01-22 22:37:39 +0100

    relative imports required

--- tests/test_task_c.py
+++ tests/test_task_c.py
@@ -1,7 +1,7 @@
 import pytest
 
-from task_a import run_sequence
-from task_c import run_sequence_with_buffer
+from ..task_a import run_sequence
+from ..task_c import run_sequence_with_buffer
 
 
 @pytest.mark.parametrize('m', [1, 2, 3, 100, 10000])
