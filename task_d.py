c7ceecf8bf3a8fb037e9f8bcb35b36090fb14aad
Tanja Bayer <tanja.bayer@widas.de>
2020-01-23 22:52:07 +0100

    displaying the diagram

--- task_d.py
+++ task_d.py
@@ -1,12 +1,25 @@
+from pytictoc import TicToc
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
+
 
 from task_a import run_sequence
 
+
 if __name__ == '__main__':
-    m_values = range(0, 10001)
+    m_values = range(1, 10001)
+    timer = TicToc()
 
+    # Calculate time of the calculation of step b
+    timer.tic()  # Start the timer
     steps = [run_sequence(m) for m in m_values]
+    timer.toc()
 
+    # Show it python3-tk needs to be installed
     plt.plot(steps)
     plt.show()
 
+    # And save it
+    plt.plot(steps)
+    plt.savefig("digramm_task_d.png")
